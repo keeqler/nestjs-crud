ab4b0bed1a6ab563c061dc7fda4ff9a3785d497f
Keepler <arturnunespe@gmail.com>
2020-06-10 06:09:58 -0300

    Author creating validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(3333);
 }
--- src/resources/authors/dto/create-author.dto.ts
+++ src/resources/authors/dto/create-author.dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, MaxLength } from 'class-validator';
+
 export class CreateAuthorBodyDto {
+  @IsNotEmpty()
+  @MaxLength(30)
   readonly name: string;
 }
